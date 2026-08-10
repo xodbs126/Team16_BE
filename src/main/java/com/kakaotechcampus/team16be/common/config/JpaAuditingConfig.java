db24cb4d540e38b5aeb1a52a844c3f31c664eeca
xodbs507 <xodbs507@gmail.com>
2026-08-10 11:51:50 +0900

    fix:@JpaAuditing 설정변경

--- src/main/java/com/kakaotechcampus/team16be/common/config/JpaAuditingConfig.java
+++ src/main/java/com/kakaotechcampus/team16be/common/config/JpaAuditingConfig.java
@@ -1,4 +1,9 @@
 package com.kakaotechcampus.team16be.common.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
 public class JpaAuditingConfig {
 }
